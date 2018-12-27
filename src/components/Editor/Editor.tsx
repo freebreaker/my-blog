50f9a61799c435a1998144b81b5bba6c5f5a2544
freebreaker <592378845@qq.com>
2018-12-27 17:31:06 +0800

    d

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import './Editor.css';
 
+interface EditorState {
+  title:string
+}
+
 export default class Editor extends React.Component{
-  constructor(props:any) {
-    super(props);
-    this.state={
-      text:"",
-      title:""
-    }
+
+  public state:Readonly<EditorState> = {
+    title:""
   }
 
   public handleTitle = (value:any)=>{
@@ -23,12 +24,10 @@ export default class Editor extends React.Component{
   };
 
   public render() {
-    
-
     return (
       <div>
         <div className="Editor">
-          {/* <input className="ArticleTitle" type="text" onChange={this.handleTitle} value={this.state.title}/> */}
+          <input className="ArticleTitle" type="text" onChange={this.handleTitle} placeholder="请输入标题"/>
           <SimpleMDE
             onChange={this.handleChange}
             options={{
